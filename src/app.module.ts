a3bf657fad718adc5457aeeee10eec4e3729e630
ehsan munna <grmunnabd@gmail.com>
2023-03-18 08:40:38 +0600

    Database connecect: database connect successfully with env.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,28 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MockModule } from './mock/mock.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
-const ENV = process.env.NODE_ENV || 'development';
-
-@Module({
+const ENV = process.env.NODE_ENV || 'development';@Module({
   imports: [
     MockModule,
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      name: 'default',
+      useFactory: (configService: ConfigService) => ({
+          autoLoadEntities: true,
+          keepConnectionAlive: true,
+          type: 'postgres',
+          host: configService.get('DB_HOST'),
+          port: configService.get<number>('DB_PORT'),
+          username: configService.get('DB_USERNAME'),
+          password: configService.get('DB_PASSWORD'),
+          database: configService.get('DB_NAME'),
+          migrationsRun: false
+      })
+  }),
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: `./env/${ENV}/.env`
