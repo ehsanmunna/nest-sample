05c314ec0e9b61b9c9987d326564e8130a8207f6
Ehsan Munna <ehsanul.munna@surbanajurong.com>
2023-04-21 11:57:42 +0600

    Cors Issue solved #2

--- src/main.ts
+++ src/main.ts
@@ -4,23 +4,32 @@ import { ConfigService } from '@nestjs/config';
 import { DocumentBuilder } from '@nestjs/swagger';
 import { SwaggerModule } from '@nestjs/swagger/dist';
 
-const ENV = process.env.NODE_ENV || 'development'
+const ENV = process.env.NODE_ENV || 'development';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule, { bodyParser: true });
   const configService = app.get(ConfigService);
   const port = configService.get('PORT');
 
   const config = new DocumentBuilder()
-  .setTitle('Nest API Sample')
-  .setDescription('A complete api sample using nest')
-  .setVersion('1.0')
-  // .addTag('Prods')
-  .build();
+    .setTitle('Nest API Sample')
+    .setDescription('A complete api sample using nest')
+    .setVersion('1.0')
+    // .addTag('Prods')
+    .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('doc', app, document);
 
+  /**
+   * CORS
+   */
+  app.enableCors({
+    origin: '*',
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
+
   await app.listen(port);
-  console.log(`server running on http://localhost:${port}`)
+  console.log(`server running on http://localhost:${port}`);
 }
 bootstrap();
